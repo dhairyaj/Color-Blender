04a42a8a5ba9cbdd00cada761592fdc0c03ffb14
dhairyaj <dhairya.jain.work@gmail.com>
2021-01-07 21:39:26 +0530

    Add slider value and update degree in linear gradient

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,18 @@
 const color1 = document.getElementById('color1');
 const color2 = document.getElementById('color2');
 const bodybg = document.getElementById('bodybg');
-const copyCode = document.getElementById('copytext');
+const copyCode = document.getElementById('copyCode');
+const slider = document.getElementById("linearDegree");
+const degree = document.getElementById("degreeValue");
+degree.innerHTML = slider.value;
+
+slider.oninput = function() {
+  degree.innerHTML = this.value;
+}
 
 function setGradient() {
-    bodybg.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
-    copyCode.textContent = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+    bodybg.style.background = `linear-gradient(${slider.value}deg, ${color1.value}, ${color2.value})`;
+    copyCode.textContent = `linear-gradient(${slider.value}deg, ${color1.value}, ${color2.value})`;
 }
 
 color1.addEventListener('input', setGradient);
