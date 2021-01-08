57e9e2ee54b30cea3cae8fc0abfe6d7a35d852c8
dhairyaj <dhairya.jain.work@gmail.com>
2021-01-08 19:51:17 +0530

    Update slider value with slider adjustment simultaneously

--- assets/main.js
+++ assets/main.js
@@ -6,14 +6,20 @@ const slider = document.getElementById("linearDegree");
 const degree = document.getElementById("degreeValue");
 degree.innerHTML = slider.value;
 
-slider.oninput = function() {
-  degree.innerHTML = this.value;
-}
+// slider.oninput = function() {
+//   degree.innerHTML = this.value;
+// }
 
 function setGradient() {
+
+    slider.oninput = function() {
+      degree.innerHTML = this.value;
+    }
+
     bodybg.style.background = `linear-gradient(${slider.value}deg, ${color1.value}, ${color2.value})`;
     copyCode.textContent = `linear-gradient(${slider.value}deg, ${color1.value}, ${color2.value})`;
 }
 
 color1.addEventListener('input', setGradient);
 color2.addEventListener('input', setGradient);
+slider.addEventListener('input', setGradient);
